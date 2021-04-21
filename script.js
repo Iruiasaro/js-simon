c491bc745a84b4dada9aaf5d362739bc06d6c4ab
Iruiasaro <brunettirosario@gmail.com>
2021-04-21 19:15:59 +0200

    inserimento Array e var

---
+++ script.js
@@ -0,0 +1,12 @@
+//set 3 array
+var computerNumbers = [];
+var userNumbers = [];
+var checkNumbers = [];
+
+//countDown 30 secondi
+var countDown = 30;
+
+//inserimento per 5 volte
+var cicli = 5;
+
+
